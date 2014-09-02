95646c1f424ab17476d0b2f6304c0986478c5da7
Winthrop Gillis <wgillis@bu.edu>
2014-09-02 19:59:20 -0400

    Added parameters to SoundDna, like genes, functions, and amplitude

--- dna.py
+++ dna.py
@@ -3,13 +3,29 @@
 
 class SoundDna:
     
-    self.generation = 0
-    def __init__(self, generation=0):
+    # name parameters and initialize them
+    # genes will be a dictionary containing all the parameters for each individual
+    # gene, i.e. amplitude, start time, stop time, function used, frequency
+    # each will be acessible by an index number
+    self.genes = {}
+    # amplitude of the sound at any given time
+    self.amplitude = 0
+    # functions that can be used to calculate a sound
+    self.functions = []
+    def __init__(self, genes=None):
         # TODO: figure out what parameters to add to init
-        self.generation = generation
+        
+        # populate the gene array with initial conditions if genes weren't 
+        # provided already
+        if not genes:
+            # TODO: populate array
+            self.genes = []
+        else:
+            self.genes = genes
+
 
     def Recombine(self, dna):
-
+        #TODO: handle times where the incoming dna is not the same length
         return # return a recombined set of dna
 
     def Mutate(self):
