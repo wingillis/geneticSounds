8e8402a8add8260d5345c0e73736fbe5c24011eb
Winthrop Gillis <wgillis@bu.edu>
2014-09-02 19:08:40 -0400

    Began structure for the dna file

---
+++ dna.py
@@ -0,0 +1,23 @@
+# this file contains the representation of the sound information
+# including how the 'DNA' will be merged and produce new members
+
+class SoundDna:
+    
+    self.generation = 0
+    def __init__(self, generation=0):
+        # TODO: figure out what parameters to add to init
+        self.generation = generation
+
+    def Recombine(self, dna):
+
+        return # return a recombined set of dna
+
+    def Mutate(self):
+        
+        # trying to stick to the functional programming paradigm
+        # by always returning things from functions
+        return #mutated dna
+
+    def getDna(self):
+
+        # return dna in a list format to be applied successively
--- main.py
+++ main.py
@@ -41,13 +41,40 @@ if sys.platform == 'linux':
 
 parser = argparse.ArgumentParser(
     description='Creates a sound from a genetic algorithm using a reference')
-
+# argument for the filename
 parser.add_argument('file', help='the input file for sound reference (.mp3 or .wav)')
+# argument for the time span in the file
 parser.add_argument('-t', help='time span that the sound spans in music file')
+# optional argument for the number of generations for evolution
+parser.add_argument('-g', type=int, help='[OPTIONAL] amount of generations to evolve. Default is 1000.', default=1000)
+# optional argument for the population size
+parser.add_argument('-p', type=int, default=500, help='[OPTIONAL] the population size of sound producers. Default is 500')
+
 
 args = parser.parse_args()
 
-# This is how you get the data from args
+# How you access the vars in args
+# print(args.g)
 # print(args.file)
 # print(args.t)
+# print(args.p)
+
+def main():
+    '''This is the main function, where everything will be handled'''
+    generations = args.g
+
+    populationSize = args.p
+
+    # array for holding the population
+    population = []
+
+    for generation in range(generations):
+        print('Processing generation {0}'.format(generation))
+        print('Populating...')
+        # TODO: fill array with DNA containing creatures
+        tempGeneration = []
+
+# run the program!!
+if __name__=='__main__':
+    main()
 
